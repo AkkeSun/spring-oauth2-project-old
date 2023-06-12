3b1aa00256d0398db1c1190f464521ebf29ffdd1
od <od@sweettracker.co.kr>
2023-06-13 08:59:15 +0900

    feat: SNS 연동을 위한 작업 (service 수정)

--- authorization-server/src/main/java/com/example/authorizationserver/service/CustomUserDetailService.java
+++ authorization-server/src/main/java/com/example/authorizationserver/service/CustomUserDetailService.java
@@ -1,7 +1,7 @@
 package com.example.authorizationserver.service;
 
 import com.example.authorizationserver.domain.Member;
-import com.example.authorizationserver.domain.Member_Role;
+import com.example.authorizationserver.domain.Role;
 import com.example.authorizationserver.repository.MemberRepository;
 import java.util.Collection;
 import java.util.List;
@@ -15,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @RequiredArgsConstructor
 @Service
@@ -26,16 +28,28 @@ public class CustomUserDetailService implements UserDetailsService {
 
     public Member save(Member user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        if (StringUtils.hasText(user.getSnsSecretKey())) {
+            user.setSnsSecretKey(passwordEncoder.encode(user.getSnsSecretKey()));
+        }
         return memberRepository.save(user);
     }
 
     @PostConstruct
-    public void init(){
-        if(memberRepository.findByUsername("user").isEmpty()){
+    public void init() {
+        if (memberRepository.findByUsername("user").isEmpty()) {
             Member member = new Member();
             member.setUsername("user");
             member.setPassword("1234");
-            member.setRole(Member_Role.ROLE_USER);
+            member.setRole(Role.ROLE_USER);
+            System.out.println(this.save(member));
+        }
+        if (memberRepository.findByUsername("user2").isEmpty()) {
+            Member member = new Member();
+            member.setUsername("user2");
+            member.setRole(Role.ROLE_ADMIN);
+            member.setSnsSync("google");
+            member.setSnsSecretKey("hello");
+            member.setPassword("socialLoginPassword");
             System.out.println(this.save(member));
         }
     }
@@ -45,6 +59,14 @@ public class CustomUserDetailService implements UserDetailsService {
         Member member = memberRepository.findByUsername(username).orElseThrow(
             () -> new UsernameNotFoundException("USER IS NOT EXISTS"));
         return new User(member.getUsername(), member.getPassword(), getAuthorities(member));
+    }
+
+    @Transactional
+    public UserDetails loadUserByUsernameAndSnsSync(String username, String snsSync) {
+        Member member = memberRepository.findByUsernameAndSnsSync(username, snsSync).orElseThrow(
+            () -> new UsernameNotFoundException("invalid userInfo"));
+        // 소셜로그인의 경우 유저 패스워드를 getSnsSecretKey() 로 설정
+        return new User(member.getUsername(), member.getSnsSecretKey(), getAuthorities(member));
     }
 
     private Collection<? extends GrantedAuthority> getAuthorities(Member member) {
